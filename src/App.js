71551fa2de2359dcbad418730b6335815ebef379
MihaiLuca95 <lucamihai1995@gmail.com>
2022-03-08 14:39:50 +0200

    the button has been added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import User from "./components/user/User";
+import {useState} from 'react';
 
-const App = () => (
+const App = () => {
+
+const [view, setView] = useState("compact");
+const changeViewMode = () => {
+  if(view === "compact") setView("detailed");
+  if(view === "detailed") setView("compact");
+}
+
+return (
   <div className="App">
       <User 
         userData = {{
@@ -8,9 +17,12 @@ const App = () => (
           nickname: "John Biden",
           lastOnline: "05-02-2022 18:00:00"
         }} 
-        viewMode = "compact"
-      />
+        viewMode = {view}
+      >
+        <button className="btn" type="button" onClick={changeViewMode}>Show {`${(view === 'compact')? 'more' : 'less'}`}</button>
+      </User>
   </div>
 );
+}
 
 export default App;
--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,24 +1,20 @@
-// Main User Module
-// FUNCTIONAL COMPONENTS
-
-// User Component:
-//  * user data
-//  * view modes: compact, datailed
-const User = ({userData: {avatar, nickname, lastOnline}, viewMode}) => {
+const User = ({userData: {avatar, nickname, lastOnline}, viewMode, children}) => {
 
     return (
         <div className="User">
             {(viewMode === 'compact')
                 ? ( <div>
                         <img src={avatar} alt="avatar" style={{width: "80px"}} /> 
+                        {children}
                     </div>)
                 : ( <div> 
-                        <img src={avatar} alt="avatar" />
+                        <img src={avatar} alt="avatar" width="200px" />
                         <h2>{nickname}</h2>
                         <p>{lastOnline}</p>
+                        {children}
                     </div>
                 ) 
-            }  
+            }
         </div>
     );
 }
