5f20f0c8e1baef414e34d327fef2254387d084ac
MihaiLuca95 <lucamihai1995@gmail.com>
2022-03-12 13:48:35 +0200

    Carousel

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import User from "./components/user/User";
+import Carousel from "./components/carousel/Carousel";
 
 const App = () => (
   <div className="App">
@@ -9,6 +10,17 @@ const App = () => (
           lastOnline: "05-02-2022 18:00:00"
         }} 
         viewMode = "compact"
+      />
+      <Carousel 
+        usersImg = {[
+          "/images/avatar_1.png",
+          "/images/avatar_2.png",
+          "/images/avatar_3.png",
+          "/images/avatar_4.png",
+          "/images/avatar_5.png",
+          "/images/avatar_6.png",
+          "/images/avatar_7.png"
+        ]}
       />
   </div>
 );
--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,26 +1,37 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-const User = ({userData: {avatar, nickname, lastOnline}, viewMode}) => {
-    const [chViewMode, setChViewMode] = useState(viewMode);
-    
-    return (
-        <div className="User">
-            {(chViewMode === 'compact')
-                ? ( <div>
-                        <img src={avatar} alt="avatar" style={{width: "80px"}} /> 
-                        <button onClick={ () => { setChViewMode("detailed") }}>Show more</button>
-                    </div>)
-                : ( <div> 
-                        <img src={avatar} alt="avatar" width="200px" />
-                        <h2>{nickname}</h2>
-                        <p>{lastOnline}</p>
-                        <button onClick={ () => { setChViewMode("compact") }}>Show less</button>
-                    </div>
-                ) 
-            }
+const User = ({ userData: { avatar, nickname, lastOnline }, viewMode }) => {
+  const [chViewMode, setChViewMode] = useState(viewMode);
+
+  return (
+    <div className="User">
+      {chViewMode === "compact" ? (
+        <div>
+          <img src={avatar} alt="avatar" style={{ width: "80px" }} />
+          <button
+            onClick={() => {
+              setChViewMode("detailed");
+            }}
+          >
+            Show more
+          </button>
         </div>
-    );
-}
+      ) : (
+        <div>
+          <img src={avatar} alt="avatar" width="200px" />
+          <h2>{nickname}</h2>
+          <p>{lastOnline}</p>
+          <button
+            onClick={() => {
+              setChViewMode("compact");
+            }}
+          >
+            Show less
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default User;
-
