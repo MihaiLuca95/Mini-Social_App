5ec2b696d2cd7af47cf28daaedb1b0cb1b9ae38b
MihaiLuca95 <lucamihai1995@gmail.com>
2022-03-14 16:26:24 +0200

    Fetch and Promise

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import User from "./components/user/User";
-import Carousel from "./components/carousel/Carousel";
+import User from "./components/UI/User";
+import {User as UserModel} from "./components/model/User";
+// import {getUser} from "./components/model/User";
+// import Carousel from "./components/carousel/Carousel";
+import {useEffect, useState} from 'react';
 
-const App = () => (
-  <div className="App">
-      <User 
-        userData = {{
-          avatar: "/images/avatar_1.png",
-          nickname: "John Biden",
-          lastOnline: "05-02-2022 18:00:00"
-        }} 
-        viewMode = "compact"
-      />
-      <Carousel 
-        usersImg = {[
-          "/images/avatar_1.png",
-          "/images/avatar_2.png",
-          "/images/avatar_3.png",
-          "/images/avatar_4.png",
-          "/images/avatar_5.png",
-          "/images/avatar_6.png",
-          "/images/avatar_7.png"
-        ]}
-      />
-  </div>
-);
+const App = () => {
+
+  let [user, setUser] = useState(null); 
+  
+  useEffect(() => {
+    // UserModel.get((userObject) => {
+    //   setUser(userObject) 
+    //   console.log(user);
+    // })
+    UserModel.get().then(data => {
+      let userObject = new UserModel(data.nickname, data.avatar, data.lastOnline)
+      setUser(userObject) 
+    })
+  }, [])
+
+  return (
+    <div className="App">
+        <User 
+          userObject = {user} 
+          viewMode = "compact"
+        />
+        {/* <Carousel 
+          usersImg = {[
+            "/images/avatar_1.png",
+            "/images/avatar_2.png",
+            "/images/avatar_3.png",
+            "/images/avatar_4.png",
+            "/images/avatar_5.png",
+            "/images/avatar_6.png",
+            "/images/avatar_7.png"
+          ]}
+        /> */}
+    </div>
+  );
+}
 
 export default App;
---
+++ src/components/model/User.js
@@ -0,0 +1,32 @@
+// model/User module
+class User {
+  constructor(nickname, avatar, lastOnline) {
+    this.nickname = nickname;
+    this.avatar = avatar;
+    this.lastOnline = lastOnline;
+  }
+
+  // dynamic / non-static 
+  static get() {
+    return fetch('/data/user.json')
+      .then(response => response.json())
+  }
+  // static get(cb) {
+  // fetch('/data/user.json')
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     cb(new User(data.nickname, data.avatar, data.lastOnline));
+  //   });
+  // }
+}
+
+// functional solution
+// const getUser = (cb) => {
+//   fetch('/data/user.json')
+//     .then(response => response.json())
+//     .then(data => {
+//       cb(new User(data.nickname, data.avatar, data.lastOnline));
+//     });
+// }
+
+export { User, /*getUser*/ } ;
